393d9550848a4ac005cdeab3c988d90679377a34
chan <dojsfffff@naver.com>
2021-11-04 23:14:02 +0900

    First Commit!!

---
+++ app/src/main/java/com/chancehee/unsplashapp_tutorial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chancehee.unsplashapp_tutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
