d6eaab96ef9857baad3fe06aeaf6946a198db0d9
chan <dojsfffff@naver.com>
2021-12-10 01:05:24 +0900

    Unsplash_API_tutorial(6): RX Java 개념 및 적용

--- app/src/main/java/com/chancehee/unsplashapp_tutorial/activities/PhotoCollectionActivity.kt
+++ app/src/main/java/com/chancehee/unsplashapp_tutorial/activities/PhotoCollectionActivity.kt
@@ -26,8 +26,14 @@ import com.chancehee.unsplashapp_tutorial.utils.Constant.TAG
 import com.chancehee.unsplashapp_tutorial.utils.RESPONSE_STATUS
 import com.chancehee.unsplashapp_tutorial.utils.SharedPrefManager
 import com.chancehee.unsplashapp_tutorial.utils.toSimpleString
+import com.jakewharton.rxbinding4.widget.textChanges
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_photo_collection.*
 import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.collections.ArrayList
 
 class PhotoCollectionActivity: AppCompatActivity(),
@@ -51,6 +57,9 @@ class PhotoCollectionActivity: AppCompatActivity(),
 
     // 서치뷰 에딧 텍스트
     private lateinit var mySearchViewEditText: EditText
+
+    // 옵저버를 통합 제거를 위한 CompositeDisposable
+    private var myCompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -105,7 +114,13 @@ class PhotoCollectionActivity: AppCompatActivity(),
             }
         }
 
-    } //
+    } // onCreate
+
+    override fun onDestroy() {
+        // 모두 삭제
+        this.myCompositeDisposable.clear()
+        super.onDestroy()
+    }
 
     // 검색 기록 리사이클러뷰 준비
     private fun searchHistoryRecyclerViewSetting(searchHistoryList: ArrayList<SearchData>){
@@ -155,7 +170,7 @@ class PhotoCollectionActivity: AppCompatActivity(),
                 when(hasExpaned){
                     true ->{
                         Log.d(TAG, "서치뷰 열림")
-                        linear_search_history_view.visibility = View.VISIBLE
+//                        linear_search_history_view.visibility = View.VISIBLE
                         handleSearchViewUi()
                     }
                     false ->{
@@ -167,6 +182,35 @@ class PhotoCollectionActivity: AppCompatActivity(),
 
             // 서치뷰에서 에딧텍스를 가져온다.
             mySearchViewEditText = this.findViewById(androidx.appcompat.R.id.search_src_text)
+
+            // 에딧텍스트 옵저버블
+            val editTextChangeObservable = mySearchViewEditText.textChanges()
+
+            val searchEditTextSubscription : Disposable =
+                // 옵저버블에 오퍼레이터를 추가
+                editTextChangeObservable
+                    // 글자가 입력되고 나서 0.8 초 후에 onNext 이벤트로 데이터 흘려보내기
+                    .debounce(800, TimeUnit.MILLISECONDS)
+                    // IO 쓰레드에서 돌리겠다.
+                    // Scheduler instance intended for IO-bound work.
+                    // 네트워크 요청, 파일 읽기,쓰기, 디비처리 등
+                    .subscribeOn(Schedulers.io())
+                    // 구독을 통해 이벤트 응답 받기
+                    .subscribeBy(
+                        onNext = {
+                            Log.d("RX", "onNext : $it")
+                            if (it.isNotEmpty()){
+                                searchPhotoApiCall(it.toString())
+                            }
+                        },
+                        onComplete = {
+                            Log.d("RX", "onComplete")
+                        },
+                        onError = {
+                            Log.d("RX", "onError : $it")
+                        }
+                    )
+            myCompositeDisposable.add(searchEditTextSubscription)
         }
 
 
@@ -210,6 +254,10 @@ class PhotoCollectionActivity: AppCompatActivity(),
         if(userInputText.count() == 12){
             Toast.makeText(this, "검색어는 12자 까지만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
         }
+
+//        if(userInputText.length in 1..12){
+//            searchPhotoApiCall(userInputText)
+//        }
 
         return true
     }
@@ -266,6 +314,7 @@ class PhotoCollectionActivity: AppCompatActivity(),
         handleSearchViewUi()
     }
 
+    // 사진 검색 API 호출
     private fun searchPhotoApiCall(query: String){
         RetrofitManager.instance.searchPhotos(searchTerm = query,completion = {status,list ->
             when(status){
@@ -299,7 +348,6 @@ class PhotoCollectionActivity: AppCompatActivity(),
             search_history_recycler_label.visibility = View.INVISIBLE
             clear_search_history_button.visibility = View.INVISIBLE
         }
-
     }
 
 
